a038e9ecc8c998758be0efe4332ef09e60005c70
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-25 14:44:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.className = "victory"
+newHeader.id = "victory"
+document.body.appendChild(newHeader)
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
