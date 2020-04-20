518d11abbdca759c3e6783252132f2a52577ecd2
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-04-19 23:55:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 // Write your code here!
-let main = document.getElementsByTagName('main')[0];
+
+
 main.remove();
+
 
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.innerHTML = 'You is the champion';
+newHeader.innerHTML = "is the champion";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let main = document.getElementsByTagName('main')[0];
+// main.remove();
+
+// let newHeader = document.createElement('h1');
+// newHeader.id = 'victory';
+// newHeader.innerHTML = 'You is the champion';
